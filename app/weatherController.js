b3056b5509bb1ed9f0470d94494479b76b7a8cbd
john abanto <john abanto>
2016-10-12 10:58:16 -0700

    second commit

--- app/weatherController.js
+++ app/weatherController.js
@@ -19,18 +19,38 @@
 
         // function to push our data from API
         vm.getInfo = function(cityname){
+            
+            toastr.options = {
+              "closeButton": false,
+              "debug": false,
+              "newestOnTop": false,
+              "progressBar": false,
+              "positionClass": "toast-bottom-right",
+              "preventDuplicates": false,
+              "onclick": null,
+              "showDuration": "300",
+              "hideDuration": "1000",
+              "timeOut": "5000",
+              "extendedTimeOut": "1000",
+              "showEasing": "swing",
+              "hideEasing": "linear",
+              "showMethod": "fadeIn",
+              "hideMethod": "fadeOut"    
+            }
+              toastr.success("Found your city!"); 
+
         	WeatherFactory.getWeather(cityname)
             .then(function(result){
         		vm.cityData = result;
         		vm.history.push({
         			place: result.name,
-        			dt: result.dt    
+        			dt: result.dt
 
         		})
 
         	});
         }
-        
-    }
+
+    }   
 
 })();
